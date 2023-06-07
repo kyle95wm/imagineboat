fead0484586c6d59a04cd15c1820fd06c823e926
BeanJr_YT <kyle95wm@gmail.com>
2023-06-06 21:09:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,53 +1,30 @@
+require('dotenv').config()
 const { Client, GatewayIntentBits, Collection, ActivityType } = require("discord.js")
+
 const client = new Client({
     allowedMentions: { parse: [] },
     intents: [
-        // Intents specify what the bot listens to from
-        // You can find more information about intents here:
-        // https://discordjs.guide/popular-topics/intents.html
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent
     ]
 })
 
-//const cooldowns = new Collection()
-//const cooldownTime = 3000 // 3 seconds cooldown
-
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
-	client.user.setActivity('people imagine', { type: ActivityType.Watching });
+    client.user.setActivity('people imagine', { type: ActivityType.Watching });
 })
 
 client.on("messageCreate", (message) => {
-    if(message.author.id === client.user.id) return // Ignore messages from other bots but work with jt99
-
+    if(message.author.id === client.user.id) return
     const content = message.content.toLowerCase()
     console.log(content)
-
     if (content.startsWith("imagine ") || content.startsWith("$$ imagine ") || content.startsWith("$$imagine ")) {
         const userId = message.author.id
-
-        //if (!cooldowns.has(userId)) {
-            // If the user is not on cooldown, proceed
-            //cooldowns.set(userId, Date.now())
-
-            const prefix = (content.startsWith("$$ imagine ") || content.startsWith("$$imagine ")) ? "$$ " : "";
-            const response = `${prefix}I can't even imagine ${content.substring(content.indexOf("imagine") + 8)}, bro.`;
-
-            //const response = `I can't even imagine ${content.substring(content.indexOf("imagine") + 8)}, bro.`
-            message.channel.send(response)
-
-            //setTimeout(() => {
-                // Remove the user from cooldown after the specified cooldownTime
-                //cooldowns.delete(userId)
-            //}, cooldownTime)
-        //} else {
-            // User is on cooldown
-           // message.channel.send("Hold on, bro. I need some time to recover!")
-        //}
+        const prefix = (content.startsWith("$$ imagine ") || content.startsWith("$$imagine ")) ? "$$ " : "";
+        const response = `${prefix}I can't even imagine ${content.substring(content.indexOf("imagine") + 8)}, bro.`;
+        message.channel.send(response)
     }
 })
 
-// Replace 'YOUR_TOKEN' with your Discord bot token
-client.login("YOUR_TOKEN")
+client.login(process.env.BOT_TOKEN)
